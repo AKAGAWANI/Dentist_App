c97270e3c9b5ff0d91fbae464cbdf2125f333994
rajat5090 <rajatmahajan26@gmail.com>
2022-08-07 19:26:39 +0530

    add logic for email sent for otp in /register,/otp,/getAppLink,/generateOtp

--- source/service/admin/AdminService.js
+++ source/service/admin/AdminService.js
@@ -1,51 +1,72 @@
 require('dotenv');
 const mongoose = require('mongoose');
-const encoder = require('urlencode')
+const encoder = require('urlencode');
+const { ResourceAPI } = require('../../commons/externals/externalsManager');
+const UserRepository = require('../user/UserRepository');
+const { API } = require('../../commons/config/ConfigManager');
 const {
-  ResourceAPI,
-}  = require('../../commons/externals/externalsManager');
-const {
-  API
-} = require('../../commons/config/ConfigManager');
-const { Scheduler, Context } = require('../../commons/context/pseudoUserContext');
+  Scheduler,
+  Context
+} = require('../../commons/context/pseudoUserContext');
 const repository = require('./AdminRepository');
-const { crypto: CryptoUtil,utility: GeneralUtil } = require('../../commons/util/UtilManager');
+const {
+  crypto: CryptoUtil,
+  utility: GeneralUtil
+} = require('../../commons/util/UtilManager');
 const { utility: utils } = require('../../commons/util/UtilManager');
 const envproperties = require('../../properties.json');
 const smsObj = require('../../commons/mailer/mailer.js');
 const OAuth2 = require('../../commons/auth/OAuth2');
 
-
-
-
-
-function Service () {}
+function Service() {}
 
 Service.prototype.simulateLogin = async function(username, password, resource) {
-  const url   = API.gateway.login.simulate;
-  const login = (await ResourceAPI.https.patch(url, null, { username, password, resource }) || {}).data;
+  const url = API.gateway.login.simulate;
+  const login = (
+    (await ResourceAPI.https.patch(url, null, {
+      username,
+      password,
+      resource
+    })) || {}
+  ).data;
 
   return login.data;
-}
-Service.prototype.adminCreate=async function(data){
+};
+Service.prototype.adminCreate = async function(data) {
+  const genOTP = GeneralUtil.generateNumericOTP();
 
-    const genOTP = GeneralUtil.generateNumericOTP();
+  let user = {
+    _id: new mongoose.Types.ObjectId().toHexString(),
+    mobile: CryptoUtil.encrypt(data.contact, true),
+    email: CryptoUtil.encrypt(data.email, true),
+    password: CryptoUtil.hash(data.password),
+    passwordOtp: CryptoUtil.hash(genOTP),
+    otpExpiry: new Date(),
+    updatedAt: new Date(),
+    createdAt: new Date(),
+    firstName: CryptoUtil.encrypt(
+      data.firstName == undefined ? '' : data.firstName,
+      true
+    ),
+    lastName: CryptoUtil.encrypt(
+      data.lastName == undefined ? '' : data.lastName,
+      true
+    ),
+    fullName: CryptoUtil.encrypt(
+      data.lastName == undefined
+        ? ''
+        : data.lastName + data.firstName == undefined
+        ? ''
+        : data.firstName,
+      true
+    ),
+    isAdmin: true
+  };
+  return await repository.createUser(user);
+};
 
-    let user={
-        _id:new mongoose.Types.ObjectId().toHexString(),
-        mobile:CryptoUtil.encrypt(data.contact, true),
-        email:CryptoUtil.encrypt(data.email, true),
-        password:CryptoUtil.hash(data.password),
-        passwordOtp:CryptoUtil.hash(genOTP),
-        otpExpiry:new Date(),
-        updatedAt:new Date(),
-        createdAt:new Date(),
-        firstName: CryptoUtil.encrypt(data.firstName==undefined?"":data.firstName, true),
-        lastName: CryptoUtil.encrypt(data.lastName==undefined?"":data.lastName, true),
-        fullName:CryptoUtil.encrypt(data.lastName==undefined?"":data.lastName+data.firstName==undefined?"":data.firstName, true),
-        isAdmin:true
-    }
-   return await repository.createUser(user)
-}
+Service.prototype.sendEmail = async function(email, name) {
+  UserRepository.sendOTPThroughEmail(email, name);
+};
 
 module.exports = new Service();
--- source/service/user/UserRepository.js
+++ source/service/user/UserRepository.js
@@ -1,25 +1,26 @@
-const { User, Account } = require("../../commons/models/mongo/mongodb");
+const { User, Account } = require('../../commons/models/mongo/mongodb');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+const smsObj = require('../../commons/mailer/mailer.js');
 
-
-function Repository () {}
+function Repository() {}
 
 Repository.prototype.deleteAccount = async function(_id) {
-  await User.remove({_id});
-  await Account.remove({_id});
+  await User.remove({ _id });
+  await Account.remove({ _id });
   return true;
-}
+};
 
 Repository.prototype.findUserByMobile = async function(mobile) {
   return User.findOne({ mobile: mobile }).exec();
-}
+};
 
 Repository.prototype.findUserByEmail = async function(email) {
   return User.findOne({ email }).exec();
-}
+};
 Repository.prototype.createUserWithMobile = async function(userObj) {
-  
-  const user = new User(); 
+  const user = new User();
   const account = new Account();
 
   const accountId = new mongoose.Types.ObjectId().toHexString();
@@ -32,15 +33,68 @@ Repository.prototype.createUserWithMobile = async function(userObj) {
   account.fullName = userObj.name;
   user.password = userObj.password;
   user.updatedAt = new Date();
-  user.devices= userObj.devices
+  user.devices = userObj.devices;
 
-  // PermissionTemplate Loading 
+  // PermissionTemplate Loading
   // user.apps = PermissionTemplate.apps;
 
   await user.save();
   await account.save();
 
   return accountId;
-}
+};
+Repository.prototype.sendOTPThroughEmail = async function(email, name) {
+  /*********** SENDING OTP TO THE USER'S EMAIL *********/
+  let file = ``,
+    subject = ``;
+
+  //Register template
+  if (name == 'Register') {
+    file = path.join(
+      __dirname,
+      '../../views/templates/registerEmailTemplate.html'
+    );
+    subject = 'Here comes your Signup OTP';
+  }
+
+  //Login template
+  if (name == 'Login') {
+    file = path.join(
+      __dirname,
+      '../../views/templates/loginEmailTemplate.html'
+    );
+    subject = 'Here comes your Login OTP';
+  }
+
+  //ForgetPassword template
+  if (name == 'ForgetPassword') {
+    file = path.join(
+      __dirname,
+      '../../views/templates/forgetpasswordEmailTemplate.html'
+    );
+    subject = 'Here comes your Login OTP';
+  }
+
+  //App link template
+  if (name == 'AppLink') {
+    file = path.join(
+      __dirname,
+      '../../views/templates/applinkEmailTemplate.html'
+    );
+    subject = 'Here comes the Doctor-Dentist App link for you';
+  }
+
+  await fs.readFile(file, 'utf8', async function(error, data) {
+    let template = data;
+    smsObj.sendMail2({
+      to: email,
+      body: subject,
+      template
+    });
+    if (error) {
+      throw error;
+    }
+  });
+};
 
 module.exports = new Repository();
